a7586b0913296e9f40d72b472259b53674ff4456
Mary Bratt <mbratt56@gmail.com>
2019-03-15 06:21:22 -0700

    added comments

--- pixelArtTool.js
+++ pixelArtTool.js
@@ -1,16 +1,18 @@
 
 var color = "#ffffff";
 
+//set the color to be transfered to the grid
 function setPenColor(fontColor) {
     "use strict";
     color = fontColor;
 }
 
+//pick the grid square that will be accepting the new color
 function setPixelColor(item) {
     "use strict";
     item.style.backgroundColor = color;
 }
-
+// allow user to start over
 function clearBoardOfColor(fontColor) {
     "use strict";
     location.reload();
